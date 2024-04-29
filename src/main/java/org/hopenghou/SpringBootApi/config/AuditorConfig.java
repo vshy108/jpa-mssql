a91339ffa156f55b196578d11ab31dc2a408d94f
penghou.ho <penghou.ho@techronex.com>
2024-04-29 11:28:10 +0800

    🐛 Fix createdDate and lastModifiedDate not saving

---
+++ src/main/java/org/hopenghou/SpringBootApi/config/AuditorConfig.java
@@ -0,0 +1,9 @@
+package org.hopenghou.SpringBootApi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditorConfig {
+}
--- src/main/java/org/hopenghou/SpringBootApi/entity/Location.java
+++ src/main/java/org/hopenghou/SpringBootApi/entity/Location.java
@@ -6,10 +6,12 @@ import org.hopenghou.SpringBootApi.converter.SQLServerGeographyAttributeConverte
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.geo.Point;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.*;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "location")
 public class Location {
   @Id
