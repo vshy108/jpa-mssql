f26be476fdcc94bc5a9c03a68a6f29399e358525
penghou.ho <penghou.ho@techronex.com>
2024-05-07 00:20:51 +0800

    🔥 Remove unused tests code

--- src/test/java/org/hopenghou/SpringBootApi/SpringBootApiApplicationTests.java
+++ src/test/java/org/hopenghou/SpringBootApi/SpringBootApiApplicationTests.java
@@ -1,33 +1,4 @@
 package org.hopenghou.SpringBootApi;
 
-// import static org.junit.Assert.assertEquals;
-
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-// import org.springframework.boot.test.web.client.TestRestTemplate;
-// import org.springframework.http.HttpEntity;
-// import org.springframework.http.HttpHeaders;
-// import org.springframework.http.HttpMethod;
-// import org.springframework.http.ResponseEntity;
-
-// mvn test
-// @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 class SpringBootApiApplicationTests {
-		// @Value("${api.key}")
-    // private String apiKey;
-
-    // @Test
-    // void exampleTest(@Autowired TestRestTemplate restTemplate) {
-		// 		HttpHeaders headers = new HttpHeaders();
-		// 		headers.set("X-API-KEY", apiKey);
-		// 		HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
-
-		// 		ResponseEntity<Object> response = restTemplate.exchange(
-    // 			"/api/v1/locations", HttpMethod.GET, requestEntity, Object.class);
-		// 		// NOTE: failed to import response classes
-		// 		assertEquals("Zero is one", response.getBody(), "Hello World");
-    // }
 }
